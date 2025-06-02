e0644c9a4599d2a217e99eaca6fcae76a203c8ae
Tony <infinite31336@gmail.com>
2025-06-02 15:47:46 +0800

    Create post detail page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -4,7 +4,8 @@ def starting_page(request):
     return render(request, 'blog/index.html')
 
 def posts(request):
+    print('render all post page')
     return render(request, 'blog/all-posts.html')
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, 'blog/post-detail.html')
