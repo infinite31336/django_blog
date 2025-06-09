48628540255171bd4ed79940bdcf067a9fe1cfb8
Tony <infinite31336@gmail.com>
2025-06-09 15:43:04 +0800

    Create models and update methods and htmls

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,49 +1,30 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 all_posts = [
-    {
-        'slug': 'Heiwajima-hot-spring',
-        'image': 'Heiwajima.jpg',
-        'author': 'Tony',
-        'date': date(2025, 6, 2),
-        'title': 'Heiwajima Hot Spring',
-        'excerpt': 'There is nothing more relaxing than visiting Japan and soaking in hot springs.',
-        'content': '''
-            Soak in Heiwajima hot springs
-        '''
-    },
-    {
-        'slug': 'stay-in-omo3-hotel',
-        'image': 'omo3.jpg',
-        'author': 'Tony',
-        'date': date(2025, 6, 3),
-        'title': 'OMO3 Hotel',
-        'excerpt': 'OMO3 hotel is an affordable and comfortable place to stay.',
-        'content': '''
-            Stay in OMO3 Hotel
-        '''
-    }
 ]
 
 def get_date(post):
     return post.get('date')
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    lastest_posts = sorted_posts[-2:]
+    lastest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {
         'posts': lastest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         'all_posts': all_posts,
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {
         'post': identified_post,
+        'post_tags': identified_post.tags.all()
     })
